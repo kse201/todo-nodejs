80875e3196531fd24eea93589b59143aa3ad804c
kse201 <=>
2017-09-04 15:27:57 +0900

    WIP: Implement task featureo
    - create task
    - list tasks
    - show task

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const app = require('./lib/app/index')
+const minimist = require('minimist')
+
+let argv = minimist(process.argv.slice(2), {
+ string: ["title", "description"],
+ boolean: ["done"]
+})
+app.run(argv)
